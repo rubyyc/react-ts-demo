3d018b03a5ccc418e52d2b2421a69f596e9f6488
Rubyc <759337828@qq.com>
2024-10-27 16:07:58 +0800

    配置eslint与prettier

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxBraketSameLine: false,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  parser: 'typescript'
+}
